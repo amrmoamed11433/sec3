c5c4ed48e63f5bd5f594d11c1fd4a8f6d719756c
amrmoamed11433 <102838939+amrmoamed11433@users.noreply.github.com>
2023-10-15 10:35:21 +0300

    Add files via upload

---
+++ JavaApplication5.java
@@ -0,0 +1,17 @@
+
+package javaapplication5;
+import java.util.Scanner;
+public class JavaApplication5 {
+
+    public static void main(String[] args) {
+        
+        
+        System.out.println("plese enter two number");
+        Scanner k=new Scanner(System.in);
+        int x=k.nextInt();
+        int y=k.nextInt();
+        int sum=x+y;
+        System.out.println(x+"+"+y+"="+sum);
+    }
+    
+}
